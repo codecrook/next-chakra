9b8b7025818a26ea037143aa03037fc2bc6737c3
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-10-15 01:13:04 +0530

    ADD: Heading and Text in Details

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -1,9 +1,12 @@
-import { VStack } from '@chakra-ui/react';
+import { VStack, Heading, Text } from '@chakra-ui/react';
 
 const Details = () => {
     return (
         <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
-
+            <VStack spacing={3} alignItems="flex-start">
+                <Heading size="2xl">Your details</Heading>
+                <Text>If you already have an account, click here to login</Text>
+            </VStack>
         </VStack>
     );
 }
