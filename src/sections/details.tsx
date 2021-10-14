e95d6a9335a7f96013c271042f07c933352ffa8b
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-10-15 01:33:38 +0530

    ADD: checkout form in Details section

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -1,4 +1,16 @@
-import { VStack, Heading, Text } from '@chakra-ui/react';
+import {
+    VStack,
+    Heading,
+    Text,
+    SimpleGrid,
+    GridItem,
+    FormControl,
+    FormLabel,
+    Input,
+    Select,
+    Checkbox,
+    Button
+} from '@chakra-ui/react';
 
 const Details = () => {
     return (
@@ -7,6 +19,51 @@ const Details = () => {
                 <Heading size="2xl">Your details</Heading>
                 <Text>If you already have an account, click here to login</Text>
             </VStack>
+            <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>First Name</FormLabel>
+                        <Input placeholder="John" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>Last Name</FormLabel>
+                        <Input placeholder="Doe" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <FormControl>
+                        <FormLabel>Address</FormLabel>
+                        <Input placeholder="31, Chinnappa Road, Electonic City" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>City</FormLabel>
+                        <Input placeholder="Bangalore" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>Country</FormLabel>
+                        <Select>
+                            <option value="in"> India </option>
+                            <option value="us"> USA </option>
+                            <option value="uk"> UK </option>
+                            <option value="uae"> UAE </option>
+                            <option value="ch"> China </option>
+                        </Select>
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <Checkbox defaultChecked> Ship to billing address</Checkbox>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <Button w="full">Place Order</Button>
+                </GridItem>
+
+            </SimpleGrid>
         </VStack>
     );
 }
