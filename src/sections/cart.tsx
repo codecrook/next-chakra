ae734847b0a703af66b5ecaeac2230f56573db0e
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-10-15 02:04:17 +0530

    ADD: theme switcher using useColorMode and useColorModeValue

--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -7,17 +7,23 @@ import {
     AspectRatio,
     Image,
     Stack,
-    Divider
+    Divider,
+    useColorMode,
+    useColorModeValue
 } from '@chakra-ui/react';
 
 const Cart = () => {
+    const { toggleColorMode } = useColorMode();
+    const bgColor = useColorModeValue('gray.50', 'whiteAlpha.50');
+    const secondaryTextColor = useColorModeValue('gray.600', 'gray.400');
+
     return (
-        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg={bgColor}>
             <VStack alignItems="flex-start" spacing={3}>
                 <Heading size="2xl">Your cart</Heading>
                 <Text>
                     If the price is too hard on your eyes,{' '}
-                    <Button variant="link" colorScheme="black">
+                    <Button variant="link" colorScheme="black" onClick={toggleColorMode}>
                         try changing the theme.
                     </Button>
                 </Text>
@@ -35,7 +41,7 @@ const Cart = () => {
                 >
                     <VStack w="full" spacing={0} alignItems="flex-start">
                         <Heading size="md">Penny board</Heading>
-                        <Text color="gray.600">PNYCOMP27541</Text>
+                        <Text color={secondaryTextColor}>PNYCOMP27541</Text>
                     </VStack>
                     <Heading size="sm" textAlign="end">
                         ₹119.00
@@ -44,21 +50,21 @@ const Cart = () => {
             </HStack>
             <VStack spacing={4} alignItems="stretch" w="full">
                 <HStack justifyContent="space-between">
-                    <Text color="gray.600">Subtotal</Text>
+                    <Text color={secondaryTextColor}>Subtotal</Text>
                     <Heading size="sm">₹119.00</Heading>
                 </HStack>
                 <HStack justifyContent="space-between">
-                    <Text color="gray.600">Shipping</Text>
+                    <Text color={secondaryTextColor}>Shipping</Text>
                     <Heading size="sm">₹19.99</Heading>
                 </HStack>
                 <HStack justifyContent="space-between">
-                    <Text color="gray.600">Taxes (estimated)</Text>
+                    <Text color={secondaryTextColor}>Taxes (estimated)</Text>
                     <Heading size="sm">₹23.80</Heading>
                 </HStack>
             </VStack>
             <Divider />
             <HStack justifyContent="space-between" w="full">
-                <Text color="gray.600">Total</Text>
+                <Text color={secondaryTextColor}>Total</Text>
                 <Heading size="lg">₹162.79</Heading>
             </HStack>
         </VStack>
--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -14,7 +14,7 @@ import {
 
 const Details = () => {
     return (
-        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
+        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
             <VStack spacing={3} alignItems="flex-start">
                 <Heading size="2xl">Your details</Heading>
                 <Text>If you already have an account, click here to login</Text>
